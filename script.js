35882d29a21470cd12b7eee3610d6df8e41e2143
Phagun Jain <omgill084@gmail.com>
2021-03-04 00:14:36 +0530

    added script

---
+++ script.js
@@ -0,0 +1,10 @@
+var x;
+
+function show_hidden(x){
+    if (x === 0){
+        document.getElementById("prsn1").style.display='block';
+    }
+    if (x === 1){
+        document.getElementById("prsn2").style.display='block';
+    }
+}
