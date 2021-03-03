5f75a1906f02b4d4ed9efa1585b080319b8f0df5
Phagun Jain <omgill084@gmail.com>
2021-03-04 00:19:54 +0530

    brokenfeatur

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var x;
 function show_hidden(x){
     if (x === 0){
         document.getElementById("prsn1").style.display='block';
+        document.getElementById("prsn2").style.display='none';
     }
     if (x === 1){
         document.getElementById("prsn2").style.display='block';
