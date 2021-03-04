cbc44671ed477b5a04b3908cbf0a074c7f139dd9
Phagun Jain <omgill084@gmail.com>
2021-03-04 15:08:29 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 var x;
 
 function show_hidden(x){
-    if (x === 0){
+    if (x === 1){
         document.getElementById("prsn1").style.display='block';
         document.getElementById("prsn2").style.display='none';
+        document.getElementById("prsn3").style.display='none';
+        document.getElementById("prsn4").style.display='none';
     }
-    else if (x === 1){
+    if (x === 2){
         document.getElementById("prsn2").style.display='block';
+        document.getElementById("prsn3").style.display='none';
+        document.getElementById("prsn4").style.display='none';
+    }
+    if (x === 3){
+        document.getElementById("prsn1").style.display='block';
+        document.getElementById("prsn2").style.display='block';
+        document.getElementById("prsn3").style.display='block';
+        document.getElementById("prsn4").style.display='none';
+    } 
+    if (x === 4){
+        document.getElementById("prsn1").style.display='block';
+        document.getElementById("prsn2").style.display='block';
+        document.getElementById("prsn3").style.display='block';
+        document.getElementById("prsn4").style.display='block';
     }
 }
